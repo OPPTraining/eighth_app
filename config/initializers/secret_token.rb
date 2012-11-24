f52c52add874a7be71e67ae010d96b3912b8a8e3
Patrick E. Burke <uconnfootball75@yahoo.com>
2012-11-24 17:28:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EighthApp::Application.config.secret_token = 'e89a644f669ec50021661231f25ccbfbba5c651b7974cdb5354fa49015ce2f51f9eb0b9eca3531bbb27de52fbc7c74af94ff8454865d5e4f17cb983a773aa857'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EighthApp::Application.config.session_store :cookie_store, key: '_eighth_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EighthApp::Application.config.session_store :active_record_store
