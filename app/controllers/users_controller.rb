0b036c363609a11644ab9667cea3d843b2d99b7f
fmr09001 <frank.rende@uconn.edu>
2013-04-03 15:20:42 -0400

    add user for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the OPP Training Portal!"
+      flash[:success] = "User Created!"
       redirect_to @user
     else
       render 'new'
