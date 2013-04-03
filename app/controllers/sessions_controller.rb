be18a1a4615eb9aa6fa8e19017de3103bd229d5a
fmr09001 <frank.rende@uconn.edu>
2013-04-03 17:38:02 -0400

    inactive users cannot login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,12 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) && !user.inactive?
       sign_in user
       redirect_back_or user
+    elsif user && user.authenticate(params[:session][:password]) && user.inactive?
+      flash.now[:error] = 'This user is currently inactive'
+      render 'new'
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
