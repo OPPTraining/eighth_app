8c7cb3b3b22a778feef9b90d878110d4a8f2ed4e
Patrick E. Burke <uconnfootball75@yahoo.com>
2013-04-14 19:10:50 -0400

    patevalsections3

--- app/models/coursesection.rb
+++ app/models/coursesection.rb
@@ -14,6 +14,7 @@
 class Coursesection < ActiveRecord::Base
   attr_accessible :coursesectiondate, :duration, :location, :course_id
   belongs_to :course
+  has_many :evaluations
 
   validates :course_id, presence: true
   validates :coursesectiondate, presence: true
--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,27 +2,27 @@
 #
 # Table name: evaluations
 #
-#  id         :integer          not null, primary key
-#  course_id  :integer
-#  Q1         :integer
-#  Q2         :integer
-#  Q3         :integer
-#  Q4         :integer
-#  Q5         :integer
-#  Q6         :integer
-#  Q7         :text
-#  Q8         :text
-#  Q9         :text
-#  Q10        :text
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
+#  id               :integer          not null, primary key
+#  coursesection_id :integer
+#  Q1               :integer
+#  Q2               :integer
+#  Q3               :integer
+#  Q4               :integer
+#  Q5               :integer
+#  Q6               :integer
+#  Q7               :text
+#  Q8               :text
+#  Q9               :text
+#  Q10              :text
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :integer
 #
 
 class Evaluation < ActiveRecord::Base
-  attr_accessible :Q1, :Q10, :Q2, :Q3, :Q4, :Q5, :Q6, :Q7, :Q8, :Q9, :course_id, :user_id
-  belongs_to :course
+  attr_accessible :Q1, :Q10, :Q2, :Q3, :Q4, :Q5, :Q6, :Q7, :Q8, :Q9, :coursesection_id, :user_id
+  belongs_to :coursesection
   belongs_to :user
 
-  validates :course_id, presence: true
+  validates :coursesection_id, presence: true
 end
