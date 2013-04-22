5ce6f73b84325b6c586226270802b4cbb50dd33d
Ben Climan <benjamin.climan@uconn.edu>
2013-04-22 16:06:36 -0400

    safdsg

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,1 @@
-class ReportsController < ApplicationController
 
-	before_filter :signed_in_user
-	before_filter :admin_or_supervisor
-
-  def reports
-  	@reports=Attendance.all
-  end
-
-  private
-
-  def admin_or_supervisor
-  	redirect_to (root_path) unless current_user.admin? || current_user.supervisor?
-  end
-
-end
