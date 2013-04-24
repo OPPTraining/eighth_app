43cc8d16159f4e42d924c5990100485f85e31391
fmr09001 <frank.rende@uconn.edu>
2013-04-24 13:50:09 -0400

    redirect to home after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password]) && !user.inactive?
       sign_in user
-      redirect_back_or user
+      redirect_back_or root_url
     elsif user && user.authenticate(params[:session][:password]) && user.inactive?
       flash.now[:error] = 'This user is currently inactive'
       render 'new'
